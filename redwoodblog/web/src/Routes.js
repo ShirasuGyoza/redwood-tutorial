2d2384ef3aa1e1e9614e8f2dd65530fcccbb904c
Akane Nakayama <31214487+ShirasuGyoza@users.noreply.github.com>
2021-06-17 21:31:15 +0900

    add: api

--- redwoodblog/web/src/Routes.js
+++ redwoodblog/web/src/Routes.js
@@ -7,11 +7,20 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route } from '@redwoodjs/router'
+
+import PostsLayout from 'src/layouts/PostsLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={NewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPage} name="post" />
+        <Route path="/posts" page={PostsPage} name="posts" />
+      </Set>
+      <Route path="/about" page={AboutPage} name="about" />
       <Route path="/" page={HomePage} name="home" />
       <Route notfound page={NotFoundPage} />
     </Router>
--- redwoodblog/web/src/pages/HomePage/HomePage.js
+++ redwoodblog/web/src/pages/HomePage/HomePage.js
@@ -1,18 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const HomePage = () => {
-  return (
-    <>
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
-  )
+  return <BlogLayout>This is HomePage</BlogLayout>
 }
 
 export default HomePage
